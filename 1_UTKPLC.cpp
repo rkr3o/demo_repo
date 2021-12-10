c530195eab517b7d0f5ae7c270bb4a6c793631d1
RAVI KUMAR RANA <rkoffice99@gmail.com>
2021-12-11 01:37:04 +0530

    first commit

---
+++ 1_UTKPLC.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int62_t
+#define span(a) begin(a), end(a)
+
+
+void byte()
+
+{
+        char first , second , third ;
+        cin>>first>>second>>third;
+
+        char a , b;
+        cin>>a>>b;
+
+        if(a==first && b == second)
+        {
+            cout<<a;
+        }
+        
+        else if(a==second && b == third)        {
+            cout<<a;
+        }
+
+        else
+        {
+            cout<<b;
+        }
+       
+
+}
+signed main(){
+ios_base::sync_with_stdio(false);
+cin.tie(nullptr); cout.tie(nullptr);
+int t;
+cin>>t;
+while(t--){
+byte();
+}
+}
